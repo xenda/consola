c2e5a8cb15e97b63fbc3daf7f58b51398e68ff14
Alvaro Pereyra <alvaro@xendacentral.com>
2012-06-16 16:06:05 -0500

    Updates with test

--- test/1.rb
+++ test/1.rb
@@ -1,5 +1,5 @@
 describe "getting to know you" do
-  it ", you should send us a String" do
+  it "we expected to receive a String" do
     value = $value
     value.should be_an_instance_of String
   end
